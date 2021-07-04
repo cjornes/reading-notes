8e9c3ed1e62b3788b7bc88e8955b49d952b0ccc3
Colin Jones <jonesguy333@gmail.com>
2021-07-04 00:44:50 -0500

    add stuff

--- dogescript.js
+++ dogescript.js
@@ -5,25 +5,34 @@ if (userAge <= 15){
   alert("Must have parental guidance while using this app!");
 }
 
-var ready = prompt("Are you ready for take off.. Yes? No?");
-console.log(ready);
-
-if (ready == "Yes"){
-  alert("TO THE MOON WE GO!!!");
+if (userAge >= 15){
+  alert("Welcome to the home of DOGE");
 }
-  
+
+var experienced = prompt("Do you have any experience in crypto?");
+console.log(experienced);
+
+if (experienced == "Yes"){
+  alert("Thats great!");
+}
+ 
+if (experienced == "No"){
+  alert("We can help you!");
+  window.stop();
+}
+
   //user logs onto website
   //ask user's age
   //ask user is he/she ready?
   //if user is ready they may "take off"
   //if user is not ready they may not "take off"
 
-if (ready == "No"){
-    alert("BETTER LUCK NEXT TIME");
-    window.stop();
+  var takeOff;
+  while (takeOff != "Yes") {
+    takeOff = prompt("Are you ready for take off.. Yes? No?");
   }
 
-
-  
-  
+  if(takeOff == "Yes"){
+    alert("TO THE MOON!!!");
+  }
   
